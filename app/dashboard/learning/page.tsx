3d11970f571ca78422e8c4c9b40f239ebf1c6208
KrotthY <carlospatriciovidal.c@gmail.com>
2024-04-08 14:28:30 -0400

    video

--- app/dashboard/learning/page.tsx
+++ app/dashboard/learning/page.tsx
@@ -3,19 +3,26 @@ import React from 'react'
 const LearningPrograms = () => {
   return (
     <>
-      <div className='flex flex-col  bg-gray-900'>
+      <div className='flex flex-col  '>
 
-      <h1 className='text-3xl text-center my-6'>Learn videos it´s HERE</h1>
+      <h1 className='text-3xl text-center my-6 font-semibold'>Titulo del curso</h1>
       <div className='  grid grid-cols-1 lg:grid-cols-3 min-h-screen px-2 gap-2'>
         
-        <div className='col-span-2 border-8 border-stone-50 rounded-sm flex justify-center items-center '>
+        <div className='col-span-2 border-8 border-stone-200   rounded-sm flex justify-center items-center '>
           <div className='bg-black w-full h-full flex justify-center items-center'>
             <span className='text-white'>Reproductor</span>
           </div>
         </div>
         
-        <div className='col-span-1 border-8 border-red-400 grid grid-rows-5'>
-          <div className='row-span-1 border-8 border-black '>
+        <div className='col-span-1 border-8 rounded-sm shadow border-stone-200 grid grid-rows-5'>
+          <div className='row-span-1 border-red-500 border-8 grid grid-rows-3'>
+            <div className='row-span-1 bg-green-300'>
+              
+              
+            </div>
+            <div  className='row-span-2 bg-green-900'>
+
+            </div>
 
           </div>
           <div className='row-span-4 border-8 border-yellow-200'>
